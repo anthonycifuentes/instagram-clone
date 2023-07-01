8ee76e7fc76a85575f3f62057ec5acf3e9656bd3
anthonycifuentes <anthonycifu@gmail.com>
2023-07-01 00:40:13 -0600

    Initial Commit

---
+++ InstagramClone/InstagramCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstagramCloneApp.swift
+//  InstagramClone
+//
+//  Created by Anthony Cifuentes on 1/07/23.
+//
+
+import SwiftUI
+
+@main
+struct InstagramCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
