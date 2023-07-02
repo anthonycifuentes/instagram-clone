8d7fa3a755515b2dbf5f1e367727272ad2e73c42
anthonycifuentes <anthonycifu@gmail.com>
2023-07-02 01:25:27 -0600

    first commit

---
+++ InstagramClone/Core/Components/UserStatView.swift
@@ -0,0 +1,27 @@
+//
+//  UserStat.swift
+//  InstagramClone
+//
+//  Created by Anthony Cifuentes on 1/07/23.
+//
+
+import SwiftUI
+
+struct UserStatView: View {
+    var
+    var body: some View {
+        VStack(alignment: .center) {
+            Text("3")
+                .font(.subheadline)
+                .fontWeight(.semibold)
+            Text("Posts")
+                .font(.footnote)
+        }
+    }
+}
+
+struct UserStatView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserStatView()
+    }
+}
