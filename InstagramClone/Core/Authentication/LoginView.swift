3e36363ee71d96ed19ddfbfba2b6e7853e07201a
anthonycifuentes <anthonycifu@gmail.com>
2023-07-05 00:42:28 -0600

    feat/login-view

---
+++ InstagramClone/Core/Authentication/LoginView.swift
@@ -0,0 +1,113 @@
+//
+//  LoginView.swift
+//  InstagramClone
+//
+//  Created by Anthony Cifuentes on 4/07/23.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    @State private var email = ""
+    @State private var password = ""
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                Image("instagram-logo-black")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 228, height: 100)
+                
+                VStack{
+                    TextField("Email", text: $email)
+                        .autocapitalization(.none)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .padding(.horizontal, 24)
+                    
+                    SecureField("Password", text: $password)
+                        .font(.subheadline)
+                        .padding(12)
+                        .background(Color(.systemGray6))
+                        .cornerRadius(8)
+                        .padding(.horizontal, 24)
+                }
+                Button {
+                    print("Forgot Password?")
+                } label: {
+                    Text("Forgot Password?")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .padding(.vertical)
+                        .padding(.trailing, 24)
+                }
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                
+                Button {
+                    print("Log In")
+                } label: {
+                    Text("Log In")
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 44)
+                        .background(Color(.systemBlue))
+                        .cornerRadius(8)
+                }
+                
+                HStack {
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                    Text("OR")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                    Rectangle()
+                        .frame(width: (UIScreen.main.bounds.width / 2) - 40, height: 0.5)
+                }
+                .foregroundColor(.gray)
+                .padding(.vertical)
+                
+                HStack {
+                    Image("facebook-logo")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 20, height: 20)
+                    Text("Continue with Facebook")
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(Color(.systemBlue))
+                }
+                Spacer()
+                
+                Divider()
+                NavigationLink {
+                    
+                } label: {
+                    HStack (spacing: 3) {
+                        Text("Don't have an account?")
+                        Text("Sign Up")
+                            .fontWeight(.semibold)
+                    }
+                    .font(.footnote)
+                    .foregroundColor(Color(.systemBlue))
+                    .padding(.vertical)
+                }
+              
+                
+                
+                
+            }
+        }
+        
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
